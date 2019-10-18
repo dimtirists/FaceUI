74e36b30d4e660a663c7460110e1a6e75d11c35f
dimitris <dimirtis@micro.gr>
2019-10-18 16:09:38 +0300

    Framework

--- UIFace/UIFace-Global/MainWindow.xaml.cs
+++ UIFace/UIFace-Global/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows.Controls;
 
 namespace UIFace_Global
 {
